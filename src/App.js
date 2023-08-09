6c1f3147f69af9beb2fae2b1868fe13e290c68b2
Diego Giraldo <dgiraldogaleano@gmail.com>
2023-08-09 18:57:58 -0500

    Todo item

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      <TodoItem/>
+      <TodoItem/>
+      <TodoItem/>
+      <TodoItem/>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,11 +21,22 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendamos React
         </a>
       </header>
     </div>
   );
 }
+
+function TodoItem() {
+  return (
+    <li>
+      <span>V</span>
+      <p>Llorar por el trabajo</p>
+      <span>X</span>
+    </li>
+  )
+}
+
 
 export default App;
