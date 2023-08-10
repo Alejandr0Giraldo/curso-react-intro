d9e29b63500ed5a8746570504c0989d5e28481ef
Diego Giraldo <dgiraldogaleano@gmail.com>
2023-08-10 18:28:12 -0500

    Completar todo

--- src/App.js
+++ src/App.js
@@ -1,38 +1,36 @@
+import React from 'react';
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
-import React from 'react';
 
-const defaultTools = [
-  {text: 'Cortar cebolla', completed: false},
-  {text: 'Tomar curso de intro react.js', completed: true},
-  {text: 'Llorar por el trabajo', completed: false},
-  {text: 'Renunciar', completed: false},
-]
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Tomar el Curso de Intro a React.js', completed: false },
+  { text: 'Llorar con la Llorona', completed: false },
+  { text: 'LALALALALA', completed: false },
+];
 
 function App() {
   return (
     <>
-
-      <TodoCounter completed={16} total={25}/>
-      <TodoSearch/>
+      <TodoCounter completed={16} total={25} />
+      <TodoSearch />
 
       <TodoList>
-        {defaultTools.map(todo => (
-          <TodoItem 
-          key={todo.text}
-          text={todo.text}
-          completed={todo.completed}
+        {defaultTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
           />
         ))}
       </TodoList>
-
-    <CreateTodoButton/> 
+      
+      <CreateTodoButton />
     </>
   );
 }
-
 
 export default App;
--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,16 +1,11 @@
-import './TodoCounter.css'
+import './TodoCounter.css';
 
-function TodoCounter({total, completed}) {
-    return (
-      <h1 style={{
-        fontSize: '24px',
-        textAlign: 'center',
-        margin: '0',
-        padding: '48px'
-      }}>
-        Has completado {completed} de {total} TODOs
-      </h1>
-    )
-  }
+function TodoCounter({ total, completed }) {
+  return (
+    <h1 className="TodoCounter">
+      Has completado <span>{completed}</span> de <span>{total}</span> TODOs
+    </h1>
+  );
+}
 
-export { TodoCounter }
+export { TodoCounter };
